17d37e1148b97683285200d3c76c6929fcf38479
dinesh24murali <dinesh24murali@gmail.com>
2023-01-22 08:47:45 +0530

    Add: SCSS support;

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
+	import './styles.scss';
 </script>
 
 <div class="app">
--- frontend/src/routes/Counter.svelte
+++ frontend/src/routes/Counter.svelte
@@ -36,9 +36,14 @@
 			<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
 		</svg>
 	</button>
+	<div class="testing">
+		<div class="tester">
+			Train running
+		</div>
+	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.counter {
 		display: flex;
 		border-top: 1px solid rgba(0, 0, 0, 0.1);
@@ -46,20 +51,21 @@
 		margin: 1rem 0;
 	}
 
-	.counter button {
-		width: 2em;
-		padding: 0;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border: 0;
-		background-color: transparent;
-		touch-action: manipulation;
-		font-size: 2rem;
-	}
-
-	.counter button:hover {
-		background-color: var(--color-bg-1);
+	.counter {
+		button {
+			width: 2em;
+			padding: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border: 0;
+			background-color: transparent;
+			touch-action: manipulation;
+			font-size: 2rem;
+		}
+		button:hover {
+			background-color: var(--color-bg-1);
+		}
 	}
 
 	svg {
